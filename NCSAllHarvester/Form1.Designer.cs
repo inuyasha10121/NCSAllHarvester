9a9cac9cf7980d3cfb890a7d180fccdc37dd29a7
Jon <Jon@DESKTOP-MD1EEPM>
2017-07-09 21:17:38 -0400

    Initial prep for harvesting.  The array handling is terrible, never use it.

--- NCSAllHarvester/Form1.Designer.cs
+++ NCSAllHarvester/Form1.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBoxDataDir = new System.Windows.Forms.TextBox();
             this.buttonLoad = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
@@ -54,13 +54,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBoxDataDir
             // 
-            this.textBox1.Location = new System.Drawing.Point(76, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(599, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "G:\\Gaussian\\F-NMR\\MonteCarlo\\wb97xStudies\\4FTol-Water\\X";
+            this.textBoxDataDir.Location = new System.Drawing.Point(76, 12);
+            this.textBoxDataDir.Name = "textBoxDataDir";
+            this.textBoxDataDir.Size = new System.Drawing.Size(599, 20);
+            this.textBoxDataDir.TabIndex = 0;
+            this.textBoxDataDir.Text = "G:\\Gaussian\\F-NMR\\MonteCarlo\\wb97xStudies\\4FTol-Water\\X";
             // 
             // buttonLoad
             // 
@@ -74,16 +74,16 @@
             // 
             // chart1
             // 
-            chartArea3.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart1.Legends.Add(legend3);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(12, 242);
             this.chart1.Name = "chart1";
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            this.chart1.Series.Add(series3);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(878, 373);
             this.chart1.TabIndex = 2;
             this.chart1.Text = "chart1";
@@ -248,7 +248,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.buttonLoad);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxDataDir);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
@@ -259,7 +259,7 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxDataDir;
         private System.Windows.Forms.Button buttonLoad;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Label label1;
--- NCSAllHarvester/Form1.cs
+++ NCSAllHarvester/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,115 @@ namespace NCSAllHarvester
 {
     public partial class Form1 : Form
     {
+        //Things that must be known before the array can be made:
+        int numAtoms = 0;
+        int numFields = 0;
+        int numAngles = 0;
+
+        //[Target Atom][Tensor Component][Contribution Type][Contribution Atom][Field Strength][Angle]
+        List<List<List<List<List<List<decimal>>>>>> harvesteddata = new List<List<List<List<List<List<decimal>>>>>>(5);
+
+        string[] tensorComponents = { "Isotropic", "XX", "YX", "ZX", "YX", "YY", "ZY", "XZ", "YZ", "ZZ" };
+        string[] contributionTypes = { "Total", "SO/Para", "SO/Dia", "AO/Para", "AO/Dia" };
+
         public Form1()
         {
             InitializeComponent();
+            foreach(var tc in  tensorComponents)
+            {
+                comboBoxTensorComp.Items.Add(tc);
+            }
+            foreach(var ct in contributionTypes)
+            {
+                comboBoxContribType.Items.Add(ct);
+            }
+            comboBoxTensorComp.SelectedIndex = 0;
+            comboBoxContribType.SelectedIndex = 0;
         }
 
         private void buttonLoad_Click(object sender, EventArgs e)
         {
+            //Do some cleaning up, just in case.
+            chart1.Series.Clear();
+            comboBoxTargetAtom.Items.Clear();
+            comboBoxContribAtom.Items.Clear();
+            textBoxLabel.Clear();
 
+            //Get field strength directories
+            var fielddirs = Directory.GetDirectories(textBoxDataDir.Text);
+            numFields = fielddirs.Length;
+            foreach(var fd in fielddirs)
+            {
+                comboBoxField.Items.Add(fd.Substring(fd.LastIndexOf("\\") + 3));
+            }
+            comboBoxField.SelectedIndex = 0;
+
+            //Find out how many angles we have now
+            var tempouts = Directory.GetFiles(fielddirs[0] + "\\ncsall\\", "*.out");
+            numAngles = tempouts.Length;
+            var angles = new int[numAngles];
+            for(int i = 0; i < numAngles; ++i)
+            {
+                var nums = tempouts[i].Substring(tempouts[i].LastIndexOf("\\") + 1).Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+                angles[i] = int.Parse(nums[0]) + int.Parse(nums[1]) + int.Parse(nums[2]);
+            }
+
+            //Find out how many atoms there are
+            var atoms = new List<string>();
+            var tempdata = File.ReadAllLines(tempouts[0]);
+            var chargesec = false;
+            for(int i = 0; i < tempdata.Length; ++i)
+            {
+                if (tempdata[i].Contains("Sum of Mulliken"))
+                    break;
+                if (chargesec)
+                {
+                    atoms.Add(tempdata[i].Split((char[])null, StringSplitOptions.RemoveEmptyEntries)[1]);
+                    comboBoxTargetAtom.Items.Add(atoms.Count + "-" + atoms[atoms.Count - 1]);
+                    comboBoxContribAtom.Items.Add(atoms.Count + "-" + atoms[atoms.Count - 1]);
+                }
+                else
+                {
+                    if (tempdata[i].Contains("Mulliken atomic charges:"))
+                    {
+                        chargesec = true;
+                        ++i;
+                    }
+                }
+            }
+            numAtoms = atoms.Count;
+            comboBoxTargetAtom.SelectedIndex = 0;
+            comboBoxContribAtom.SelectedIndex = 0;
+
+            //Prep the array (Never, EVER, do this shit again.  Christ.)
+            for(int a = 0; a < numAtoms; ++a)
+            {
+                harvesteddata.Add(new List<List<List<List<List<decimal>>>>>());
+                for (int b = 0; b < 10; ++b)
+                {
+                    harvesteddata[a].Add(new List<List<List<List<decimal>>>>());
+                    for (int c = 0; c < 5; ++c)
+                    {
+                        harvesteddata[a][b].Add(new List<List<List<decimal>>>());
+                        for (int d = 0; d < numAtoms; ++d)
+                        {
+                            harvesteddata[a][b][c].Add(new List<List<decimal>>());
+                            for (int f = 0; f < numFields; ++f)
+                            {
+                                harvesteddata[a][b][c][d].Add(new List<decimal>());
+                                for(int g = 0; g < numAngles; ++g)
+                                {
+                                    harvesteddata[a][b][c][d][f].Add(0.0m);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
+            MessageBox.Show("Loaded");
         }
     }
 }
